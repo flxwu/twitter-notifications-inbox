fc97f58d7d3383dcc5b1c6a9e2d7b642a02d7e10
Felix Wu <mail@flxwu.com>
2018-12-09 12:04:55 +0100

    add notificationUsers to IUser interface

--- backend/src/interfaces/user.ts
+++ backend/src/interfaces/user.ts
@@ -1,5 +1,8 @@
+import { ITwitterUser } from '../schemas/twitApi';
+
 export interface IUser {
   name?: string;
   token?: string;
   tokenSecret?: string;
+  notificationUsers?: Array<ITwitterUser>;
 }
